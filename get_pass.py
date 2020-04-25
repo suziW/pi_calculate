81e66a70f4ca7b0aa093240700c2bdda7a7e52d3
wang <server.office.com>
2020-04-25 13:17:34 +0800

    done

--- get_pass.py
+++ get_pass.py
@@ -4,4 +4,4 @@ with open('./1000000.txt', 'r') as f:
 start = int(input('请输入想要获取多少位小数点开始的数: ')) + 1
 length = int(input('请输入想要获取多少位数据长度: '))
 
-print('结果是:{}'.format(s[start:start + length]))
+print('结果是:{}'.format(pi[start:start + length]))
